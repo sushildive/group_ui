9e9a2da915174ce74af6b86a5b9685defc5c0b33
sanganak guru <dsusheel@gmail.com>
2013-10-06 23:16:39 +0530

    Added open/close all handling

--- Devices_Groups_files/group-ui.js
+++ Devices_Groups_files/group-ui.js
@@ -39,7 +39,6 @@ var boxOperations = {
 	 * Show group box
 	 */
 	openUp : function() {
-		$(this).customScrollbar("resize");
 		$(this).slideDown();
 	},
 
@@ -60,26 +59,77 @@ var boxOperations = {
 			targetObject.openUp();
 			myDad.addClass('highlight');
 		}
+	},
+
+	toggleAllGroupDisplay : function() {
+		var state;
+		if (boxOperations.isOpenAll()) {
+			// open all
+			$('.expanding-box').not('.highlight').find('.expanding-box-handler').toggleBasicGroupDisplay();
+			state = 'cab';
+		} else {
+			// close all
+			$('.expanding-box.highlight').find('.expanding-box-handler').toggleBasicGroupDisplay();
+			state = 'oab';
+		}
+
+		$(this).updateOCAllState(state);
+	},
+
+	isOpenAll : function() {
+		return $('button[name="toggle-all-groups"]').hasClass('oab');
+	},
+
+	updateOCAllState2 : function() {
+		var closedGroupsExists = ($('.expanding-box').not('.highlight').find('.expanding-box-handler').length > 0);
+		var state;
+		if (closedGroupsExists) {
+			state = 'oab';
+		} else {
+			state = 'cab';
+		}
+
+		$(this).updateOCAllState(state);
+	},
+
+	updateOCAllState : function(state) {
+
+		var oldCls;
+		var newBtnLbl;
+		if (state == 'cab') {
+			oldCls = 'oab';
+			newBtnLbl = 'Close All';
+		} else {
+			oldCls = 'cab';
+			newBtnLbl = 'Open All';
+		}
+		$('button[name="toggle-all-groups"]').removeClass(oldCls).addClass(state).text(newBtnLbl);
 	}
 };
 
 /*
-(function($) {
-	$.fn.groupUI = function() {
-	};
-})(jQuery);*/
+ (function($) {
+ $.fn.groupUI = function() {
+ };
+ })(jQuery);*/
 
 $(function() {
 	$.extend($('.expanding-box-handler'), boxConfig);
 	$(".expanding-box-dropdown").dynatree();
 	$(".demo").customScrollbar();
 	$(".demo").customScrollbar("resize");
+
 	jQuery.fn.extend(boxOperations);
 
 	$('.expanding-box-handler').click(function() {
 		$(this).toggleBasicGroupDisplay();
+		boxOperations.updateOCAllState2();
 	});
 	$('.expanding-box-action').click(function() {
 		$(this).parent().parent().find('.expanding-box-handler').click();
+	});
+
+	$('button[name="toggle-all-groups"]').click(function() {
+		$(this).toggleAllGroupDisplay();
 	});
 });
