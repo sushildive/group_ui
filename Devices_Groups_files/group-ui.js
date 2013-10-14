c740e1e5db14e258ff4fc85da87b0ee5ed7d9d5b
sanganak guru <dsusheel@gmail.com>
2013-10-14 09:04:46 +0530

    refactored plugin to enable clients invoke exposed methods

--- Devices_Groups_files/group-ui.js
+++ Devices_Groups_files/group-ui.js
@@ -9,29 +9,78 @@
 
 			var settings = $.extend(defaultSettings, options);
 
-			return this.each(function() {
-				var boxComponent = {
-					root : $(this),
-					primaryHandler : $(this).find('.expanding-box-handler'),
-					secondaryHandler : $(this).find('.expanding-box-action'),
-					contentHolder : $(this).find('.expanding-box-dropdown')
-				};
-
-				$(this).find('.expanding-box-dropdown').dynatree({
+			var GroupBox = function(opts, ele) {
+				this.options = opts;
+				this.root = $(ele);
+				this.primaryHandler = $(ele).find('.expanding-box-handler');
+				this.secondaryHandler = $(ele).find('.expanding-box-action');
+				this.contentHolder = $(ele).find('.expanding-box-dropdown');
+				this.root.data("groupBox", this);
+				$(ele).find('.expanding-box-dropdown').dynatree({
 					onExpand : function(flag, node) {
 						resizeBox(flag, node, boxComponent);
 					}
 				});
 				// init scrollbar, dynatree
-				$(this).find('.expanding-box-dropdown').customScrollbar();
-				$(this).find('.expanding-box-handler').click(function() {
-					toggleBox(boxComponent);
+				var _this = this;
+				$(ele).find('.expanding-box-dropdown').customScrollbar();
+				$(ele).find('.expanding-box-handler').click(function() {
+					console.log(this);
+					_this.toggleBox();
 				});
 
-				$(this).find('.expanding-box-action').click(function() {
+				$(ele).find('.expanding-box-action').click(function() {
 					// click to primary box handler
 					boxComponent.primaryHandler.click();
 				});
+			};
+
+			GroupBox.prototype = {
+				toggleBox : function() {
+					var myDad = this.root;
+					var targetObject = this.contentHolder;
+					var myHeight;
+					if (myDad.hasClass('highlight')) {
+						targetObject.slideUp();
+						myDad.removeClass('highlight');
+						myHeight = 0;
+					} else {
+						targetObject.slideDown();
+						myDad.addClass('highlight');
+						myHeight = $(targetObject).find('.dynatree-container').height();
+					}
+					targetObject.height(myHeight);
+					targetObject.customScrollbar("resize");
+
+					if (settings.onBoxStateChange) {
+						settings.onBoxStateChange.call();
+					}
+				},
+				openMaximized : function() {
+					// TODO implement
+					console.log('openMaximized not yet implemented');
+					console.log(this);
+				},
+				closeMinimized : function() {
+					// TODO implement
+					console.log('closeMinimized not yet implemented');
+					console.log(this);
+				}
+			};
+
+			return this.each(function() {
+				if (options == undefined)
+					options = defaultSettings;
+				if ( typeof (options) == "string") {
+					var gb = $(this).data("groupbox");
+					if (gb)
+						gb[options](args);
+				} else if ( typeof (options) == "object") {
+					options = $.extend(defaultSettings, options);
+					new GroupBox(options, $(this));
+				} else
+					throw "Invalid type of options";
+
 			});
 
 			function toggleBox(boxObject) {
@@ -55,14 +104,39 @@
 				}
 			}
 
-			function resizeBox(flag, node, boxObject) {
+			function resizeBox(boxObject) {
 				// TODO implement box resize code
 				if (settings.onBoxSizeChange) {
 					settings.onBoxSizeChange.call();
 				}
 			}
 
+			function maximize(boxObject) {
+				//TODO implement extend logic here
+
+			}
+
+			function restore(boxObject) {
+				// TODO implement restore logic here
+			}
+
 		};
+
+		$.fn.groupUI.openMaximized = function() {
+			// TODO implement open all feature
+			console.log('openMaximized not yet implemented');
+			console.log(this);
+		};
+
+		$.fn.groupUI.closeMinimized = function() {
+			// TODO implement close all
+			console.log('closeMinimized not yet implemented');
+		};
+
+		/*
+		 $("#tree").dynatree("getRoot").visit(function(node) {
+		 node.expand(true);
+		 });*/
 
 	}(jQuery));
 /**
@@ -136,4 +210,8 @@ $(function() {
 			boxOperations.updateOCAllUI();
 		}
 	});
+	$('button[name="toggle-all-groups"]').click(function() {
+		$('.expanding-box').groupUI('openMaximized');
+	});
 });
+
